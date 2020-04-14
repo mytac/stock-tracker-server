f5ad025ef69eb87f9e5656c054b1c0392a004b08
mytac <mytac1995@gmail.com>
2020-04-14 08:39:30 +0800

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+/***
+ * @Description: 
+ * @Author: mytac
+ * @Date: 2020-04-11 21:40:19
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/login', function(req, res, next) {
+  console.log(req.body)
+  res.send('login');
+});
+
+
+module.exports = router;
